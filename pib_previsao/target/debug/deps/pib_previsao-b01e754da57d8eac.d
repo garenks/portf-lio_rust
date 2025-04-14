48f92a73a5376c602fd72067f130093e1188d698
Paulo <88458042+garenks@users.noreply.github.com>
2025-04-14 21:20:44 +0000

    teste

--- pib_previsao/target/debug/deps/pib_previsao-b01e754da57d8eac.d
+++
@@ -1,5 +0,0 @@
-/workspaces/portf-lio_rust/pib_previsao/target/debug/deps/pib_previsao-b01e754da57d8eac: src/main.rs
-
-/workspaces/portf-lio_rust/pib_previsao/target/debug/deps/pib_previsao-b01e754da57d8eac.d: src/main.rs
-
-src/main.rs:
--- pib_previsao/target/debug/pib_previsao.d
+++
@@ -1,1 +0,0 @@
-/workspaces/portf-lio_rust/pib_previsao/target/debug/pib_previsao: /workspaces/portf-lio_rust/pib_previsao/src/main.rs
